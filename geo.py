93cbfb6aefaf54bbd5d2d5b7b5f49958a0e6004d
Richard Klem <rklem@codasip.com>
2021-01-15 14:52:13 +0100

    Functionality all done. Need labels size etc.

--- geo.py
+++ geo.py
@@ -9,11 +9,13 @@ import contextily as ctx
 import sklearn.cluster
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering
 import seaborn
 import descartes
 import cartopy.crs as ccrs
 
+import scipy.stats
+from scipy.stats import chi2_contingency
 
 # muzeze pridat vlastni knihovny
 
@@ -56,7 +58,8 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     df = optimize_dataframe(df)
     df.drop(df[np.isnan(df.d)].index, inplace=True)
     df.drop(df[np.isnan(df.e)].index, inplace=True)
-    df.info = df[df['region'] == 'JHM']
+    df = df[df['region'] == 'JHM']
+    df.drop(df[df.d == -703607.94].index, inplace=True)  # Delete the outlier point.
 
     geo_df = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["d"], df["e"]),
                                     crs="EPSG:5514")
@@ -73,6 +76,7 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     gdf_outside = gdf[gdf["p5a"] == 2]
     plots = [gdf_inside, gdf_outside]
     colours = ["tab:orange", "tab:red"]
+    titles = ['Nehody v Jihomoravském kraji v obci', 'Nehody v Jihomoravském kraji mimo obec']
 
     for i, ax in enumerate(axes):
         plots[i].plot(ax=ax, markersize=3, color=colours[i])
@@ -82,12 +86,14 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                 source=ctx.providers.Stamen.TonerLite,
                 )
         gdf.boundary.plot(ax=ax, color="k")
+        ax.set_title(titles[i])
         ax.axis("off")
 
     if fig_location:
         touch(fig_location)
         plt.savefig(fig_location)
     if show_figure:
+        plt.tight_layout()
         plt.show()
 
 
@@ -98,53 +104,46 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     a = pd.Series(gdf['geometry'].apply(lambda p: p.x))
     b = pd.Series(gdf['geometry'].apply(lambda p: p.y))
     X = np.column_stack((a, b))
-    n_clusters = 10
+    n_clusters = 14
 
-    # fig, ax = plt.subplots(1, 1, figsize=(20, 15))
-    # gdf.plot(column='cluster', ax=ax, markersize=3)
-
-    kmeans = KMeans(n_clusters=n_clusters, random_state=5)
+    kmeans = KMeans(n_clusters=n_clusters)
     labels = pd.Series(kmeans.fit_predict(X))
 
     gdf['cluster'] = labels.values
-    # bin by cluster
     cluster_size: pd.DataFrame = gdf.groupby('cluster').cluster.count()
-
-    # plot, using #circles / per cluster as the od weight
-    clusters = kmeans.cluster_centers_
-
-    print(type(clusters), clusters)
-    print(type(np.array([cluster_size.to_numpy()])), np.array([cluster_size.to_numpy()]))
+    clusters: np.ndarray = kmeans.cluster_centers_
 
     clusters = np.hstack((clusters, np.array([cluster_size.to_numpy()]).transpose()))
-    # np.append(clusters, np.array([cluster_size.to_numpy().transpose()]), axis=1)
-    fig, ax = plt.subplots(1, 1, figsize=(15, 12))
+    fig, ax = plt.subplots(1, 1, figsize=(20, 15))
     gdf.plot(ax=ax, markersize=1, color="tab:blue")
-    plt.scatter(x=clusters[:, 0], y=clusters[:, 1],  # clusters x,y
-                c=cluster_size,  # color
-                s=clusters[:, 2] * 0.7,  # diameter, scaled
-                facecolors='none',  # don't fill markers
-                alpha=0.5)
-
+    plt.scatter(x=clusters[:, 0],
+                y=clusters[:, 1],
+                c=cluster_size,
+                s=clusters[:, 2] * 0.8,
+                facecolors='none',
+                alpha=0.6)
     ctx.add_basemap(
             ax,
             crs=gdf.crs.to_string(),
             source=ctx.providers.Stamen.TonerLite,
             )
     gdf.boundary.plot(ax=ax, color="k")
-    plt.colorbar()
+    color_bar = plt.colorbar(shrink=0.65)
+    color_bar.set_alpha(1)
+    color_bar.draw_all()
+    ax.set_title('Nehody v Jihomoravském kraji')
     ax.axis("off")
-    plt.show()
 
     if fig_location:
         touch(fig_location)
         plt.savefig(fig_location)
     if show_figure:
+        plt.tight_layout()
         plt.show()
 
 
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
-    # plot_geo(gdf, "geo1.png", True)
+    plot_geo(gdf, "geo1.png", True)
     plot_cluster(gdf, "geo2.png", True)
